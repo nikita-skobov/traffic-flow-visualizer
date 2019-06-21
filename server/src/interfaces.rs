1d27ec923f61b7d40dc65f606870bac4cecd8c35
nikita-skobov <nskobov.websites@gmail.com>
2019-06-20 20:38:14 -0500

    formats with rustfmt

--- server/src/interfaces.rs
+++ server/src/interfaces.rs
@@ -4,70 +4,72 @@ use pnet::datalink::NetworkInterface;
 use std::io;
 
 pub fn show_interfaces() -> Option<Vec<NetworkInterface>> {
-  let interfaces = datalink::interfaces();
-  let mut index = 0;
+    let interfaces = datalink::interfaces();
+    let mut index = 0;
 
-  let num_interfaces = interfaces.len();
-  if num_interfaces == 0 {
-    None
-  } else {
-    println!("CHOOSE WHICH INTERFACE TO MONITOR: (enter a number [{} - {}])\n\n",
-      0,
-      num_interfaces - 1);
+    let num_interfaces = interfaces.len();
+    if num_interfaces == 0 {
+        None
+    } else {
+        println!(
+            "CHOOSE WHICH INTERFACE TO MONITOR: (enter a number [{} - {}])\n\n",
+            0,
+            num_interfaces - 1
+        );
 
-    for interface in &interfaces {
-      let ip_str = interface.ips
-        .iter()
-        .map(|ip| {
-          format!("{} ", ip.to_string())
-        })
-        .collect::<String>();
-      
-      println!("[{}]", index);
-      println!("NAME: {}", interface.name);
-      println!("MAC: {:?}", interface.mac_address());
-      println!("IP(s): {}", ip_str);
-      println!("FLAGS: {}\n", interface.flags);
+        for interface in &interfaces {
+            let ip_str = interface
+                .ips
+                .iter()
+                .map(|ip| format!("{} ", ip.to_string()))
+                .collect::<String>();
 
-      index += 1;
+            println!("[{}]", index);
+            println!("NAME: {}", interface.name);
+            println!("MAC: {:?}", interface.mac_address());
+            println!("IP(s): {}", ip_str);
+            println!("FLAGS: {}\n", interface.flags);
+
+            index += 1;
+        }
+
+        Some(interfaces)
     }
-
-    Some(interfaces)
-  }
 }
-
 
 pub fn pick_interface(interfaces: Vec<NetworkInterface>) -> NetworkInterface {
     let min_interface_index = 0;
     let max_interface_index = interfaces.len() as i32 - 1;
-    println!("CHOOSE WHICH INTERFACE TO MONITOR: (enter a number [{} - {}])\n\n",
-      min_interface_index,
-      max_interface_index);
+    println!(
+        "CHOOSE WHICH INTERFACE TO MONITOR: (enter a number [{} - {}])\n\n",
+        min_interface_index, max_interface_index
+    );
 
     let mut input_number: i32 = -1;
 
     while input_number < min_interface_index || input_number > max_interface_index {
-      let mut input_number_str = String::new();
-      io::stdin()
-        .read_line(&mut input_number_str)
-        .expect("Failed to read from stdin");
+        let mut input_number_str = String::new();
+        io::stdin()
+            .read_line(&mut input_number_str)
+            .expect("Failed to read from stdin");
 
-      input_number_str = input_number_str.trim().to_string();
-      input_number = match input_number_str.parse::<i32>() {
-        Ok(i) => {
-          if (input_number < min_interface_index || input_number > max_interface_index) {
-            println!("Please pick a number between {} and {}\n", min_interface_index, max_interface_index);
-          }
-          i
-        },
-        Err(..) => {
-          println!("Error, {} is not a number\n", input_number_str);
-          -1
-        },
-      };
-
+        input_number_str = input_number_str.trim().to_string();
+        input_number = match input_number_str.parse::<i32>() {
+            Ok(i) => {
+                if (input_number < min_interface_index || input_number > max_interface_index) {
+                    println!(
+                        "Please pick a number between {} and {}\n",
+                        min_interface_index, max_interface_index
+                    );
+                }
+                i
+            }
+            Err(..) => {
+                println!("Error, {} is not a number\n", input_number_str);
+                -1
+            }
+        };
     }
-
 
     interfaces[input_number as usize].clone()
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -10,23 +10,21 @@ fn main() {
     // -d --delay (in milliseconds, how much to delay the main loop by. to prevent too much cpu usage)
     // -ao --always-on (by default, should only run the main loop if someone is connected to the websocket server, ao cancels this out)
 
-
     // show user list of interfaces
     let interface_vec = match interfaces::show_interfaces() {
-      Some(i) => i,
-      None => {
-        println!("No interfaces found");
-        std::process::exit(1);
-      },
+        Some(i) => i,
+        None => {
+            println!("No interfaces found");
+            std::process::exit(1);
+        }
     };
 
     // ask user to pick one by entering the number in the console
     let interface = interfaces::pick_interface(interface_vec);
 
-
     // start webserver, and websocket server
 
-    // start 
+    // start
     // let interfaces = datalink::interfaces();
     // println!("INTERFACE #0: {:?}", interfaces[0]);
     // for interface in datalink::interfaces() {
