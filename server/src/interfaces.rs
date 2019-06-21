0ab3bf8333d957b6e4127e3a5a89867617c4d57e
nikita-skobov <nskobov.websites@gmail.com>
2019-06-20 20:36:05 -0500

    moves pick interface to seperate function in interface module

--- server/src/interfaces.rs
+++ server/src/interfaces.rs
@@ -35,3 +35,39 @@ pub fn show_interfaces() -> Option<Vec<NetworkInterface>> {
     Some(interfaces)
   }
 }
+
+
+pub fn pick_interface(interfaces: Vec<NetworkInterface>) -> NetworkInterface {
+    let min_interface_index = 0;
+    let max_interface_index = interfaces.len() as i32 - 1;
+    println!("CHOOSE WHICH INTERFACE TO MONITOR: (enter a number [{} - {}])\n\n",
+      min_interface_index,
+      max_interface_index);
+
+    let mut input_number: i32 = -1;
+
+    while input_number < min_interface_index || input_number > max_interface_index {
+      let mut input_number_str = String::new();
+      io::stdin()
+        .read_line(&mut input_number_str)
+        .expect("Failed to read from stdin");
+
+      input_number_str = input_number_str.trim().to_string();
+      input_number = match input_number_str.parse::<i32>() {
+        Ok(i) => {
+          if (input_number < min_interface_index || input_number > max_interface_index) {
+            println!("Please pick a number between {} and {}\n", min_interface_index, max_interface_index);
+          }
+          i
+        },
+        Err(..) => {
+          println!("Error, {} is not a number\n", input_number_str);
+          -1
+        },
+      };
+
+    }
+
+
+    interfaces[input_number as usize].clone()
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,4 @@
 use pnet::datalink;
-use std::io;
 
 mod interfaces;
 
@@ -12,7 +11,8 @@ fn main() {
     // -ao --always-on (by default, should only run the main loop if someone is connected to the websocket server, ao cancels this out)
 
 
-    let interfaces = match interfaces::show_interfaces() {
+    // show user list of interfaces
+    let interface_vec = match interfaces::show_interfaces() {
       Some(i) => i,
       None => {
         println!("No interfaces found");
@@ -20,44 +20,15 @@ fn main() {
       },
     };
 
-    // show user list of interfaces
     // ask user to pick one by entering the number in the console
-    let interfaces = datalink::interfaces();
-    let min_interface_index = 0;
-    let max_interface_index = interfaces.len() as i32 - 1;
-    println!("CHOOSE WHICH INTERFACE TO MONITOR: (enter a number [{} - {}])\n\n",
-      min_interface_index,
-      max_interface_index);
-
-    let mut input_number: i32 = -1;
-
-    while input_number < min_interface_index || input_number > max_interface_index {
-      let mut input_number_str = String::new();
-      io::stdin()
-        .read_line(&mut input_number_str)
-        .expect("Failed to read from stdin");
-      
-      input_number_str = input_number_str.trim().to_string();
-      input_number = match input_number_str.parse::<i32>() {
-        Ok(i) => i,
-        Err(..) => {
-          println!("Error, {} is not a number\n", input_number_str);
-          -1
-        },
-      };
-
-      if (input_number < min_interface_index || input_number > max_interface_index) {
-        println!("Please pick a number between {} and {}\n", min_interface_index, max_interface_index);
-      }
-    }
-
+    let interface = interfaces::pick_interface(interface_vec);
 
 
     // start webserver, and websocket server
 
     // start 
-    let interfaces = datalink::interfaces();
-    println!("INTERFACE #0: {:?}", interfaces[0]);
+    // let interfaces = datalink::interfaces();
+    // println!("INTERFACE #0: {:?}", interfaces[0]);
     // for interface in datalink::interfaces() {
     //     println!("{}", interface.name);
     // }
