4130b6a8d0b91408c0fb39ee0d18970453e8353c
nikita-skobov <nskobov.websites@gmail.com>
2019-06-24 19:11:06 -0500

    adds reducer cases for ws connection closed, and failure

--- ui/src/reducers/connectionReducer.js
+++ ui/src/reducers/connectionReducer.js
@@ -1,17 +1,45 @@
 import {
   WS_CONNECTION_ESTABLISHED,
+  WS_CONNECTION_BEGIN,
+  WS_CONNECTION_CLOSED,
+  WS_CONNECTION_FAILED,
+  CONNECTION_STATE_ON,
+  CONNECTION_STATE_IN_PROGRESS,
+  CONNECTION_STATE_OFF,
 } from '../constants'
 
 
 export const defaultConnectionState = {
   ws: 'ws://localhost:3012',
   isConnected: false,
+  connectionState: CONNECTION_STATE_OFF,
 }
 export function connectionReducer(state = defaultConnectionState, action) {
   switch (action.type) {
+    case WS_CONNECTION_BEGIN: {
+      const retObj = { ...state }
+      retObj.connectionState = CONNECTION_STATE_IN_PROGRESS
+      return retObj
+    }
+
     case WS_CONNECTION_ESTABLISHED: {
       const retObj = { ...state }
       retObj.isConnected = true
+      retObj.connectionState = CONNECTION_STATE_ON
+      return retObj
+    }
+
+    case WS_CONNECTION_CLOSED: {
+      const retObj = { ...state }
+      retObj.isConnected = false
+      retObj.connectionState = CONNECTION_STATE_OFF
+      return retObj
+    }
+
+    case WS_CONNECTION_FAILED: {
+      const retObj = { ...state }
+      retObj.isConnected = false
+      retObj.connectionState = CONNECTION_STATE_OFF
       return retObj
     }
 
